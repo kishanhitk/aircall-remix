79cf46187247d9d23fc565c18804160f2e9b5414
kishanhitk <kishansharma1231@gmail.com>
2024-01-04 01:03:13 +0530

    fix: sticky navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import { HeaderLink } from "./HeaderLink";
 
 const Header = () => {
   return (
-    <div className="flex max-w-2xl mx-auto border-b-2 border-gray-100 sticky">
+    <div className="flex max-w-2xl mx-auto border-b-2 border-gray-100 sticky top-0 bg-white z-10">
       <HeaderLink href="/activities" title="Activities" />
       <HeaderLink href="/archived" title="Archived" />
     </div>
