78cb529783af7f0f25a696b862826ccc59e3a693
kishanhitk <kishansharma1231@gmail.com>
2024-01-04 01:06:22 +0530

    feat: improve backdrop blur on header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import { HeaderLink } from "./HeaderLink";
 
 const Header = () => {
   return (
-    <div className="flex max-w-2xl mx-auto border-b-2 border-gray-100 sticky top-0 bg-white z-10">
+    <div className="flex max-w-2xl mx-auto border-b-2 border-gray-100 sticky top-0 bg-white/50 z-10 backdrop-blur-md">
       <HeaderLink href="/activities" title="Activities" />
       <HeaderLink href="/archived" title="Archived" />
     </div>
